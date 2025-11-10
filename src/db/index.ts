bd5a93b88bb4775e99c15d232ba4e2b65a8c3516
DennisJosefsson <dennis.josefsson@gmail.com>
2025-11-10 10:46:15 +0100

    Removed db server function

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,6 @@ import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
 
-import { createServerOnlyFn } from '@tanstack/react-start'
 import * as schema from './schema.ts'
 
 config()
@@ -17,7 +16,3 @@ const pool = new Pool({
   connectionString: dbConnectionString,
 })
 export const db = drizzle(pool, { schema })
-
-export type DB = typeof db
-
-export const dbServerConnection = createServerOnlyFn(() => db)
