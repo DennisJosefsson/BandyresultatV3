2c3a1511578b44eb6c29317cbc7f4c8397ffbb6e
DennisJosefsson <dennis.josefsson@gmail.com>
2025-11-25 16:05:15 +0100

    Added teams list

---
+++ src/routes/_layout/teams/-components/TeamsList/TeamsListItem.tsx
@@ -0,0 +1,44 @@
+import { Checkbox, CheckedState } from '@/components/ui/checkbox'
+import { cn } from '@/lib/utils'
+
+type Team = {
+  teamId: number
+  casualName: string
+}
+
+type TeamsListItemProps = {
+  team: Team
+  selectedTeams: number[]
+  onCheckedChange: (checked: CheckedState, teamId: number) => void
+}
+
+const TeamsListItem = ({
+  team,
+  selectedTeams,
+  onCheckedChange,
+}: TeamsListItemProps) => {
+  //const { favTeams } = useTeampreferenceContext()
+  //   const pathName = useLocation().pathname
+  //   const women = useSearch({
+  //     from: '/_layout',
+  //     select: (search) => search.women,
+  //   })
+
+  return (
+    <div className="has-data-[state=checked]:font-bold flex flex-row items-center justify-between space-x-3 space-y-0 rounded bg-muted p-2 text-sm dark:bg-muted/50 md:text-base 2xl:text-lg">
+      <span className={cn('w-32 peer-data-[state=checked]:underline')}>
+        {team.casualName}
+      </span>
+      <Checkbox
+        name="teamArray"
+        checked={selectedTeams.includes(team.teamId)}
+        onCheckedChange={(checked) =>
+          team.teamId && onCheckedChange(checked, team.teamId)
+        }
+        className="peer bg-muted data-[state=checked]:border-primary data-[state=checked]:bg-background data-[state=checked]:text-primary dark:bg-muted/50 dark:data-[state=checked]:border-white dark:data-[state=checked]:bg-background dark:data-[state=checked]:text-primary"
+      />
+    </div>
+  )
+}
+
+export default TeamsListItem
--- src/routes/_layout/teams/index.tsx
+++ src/routes/_layout/teams/index.tsx
@@ -1,8 +1,10 @@
+import Loading from '@/components/Loading/Loading'
 import { createFileRoute } from '@tanstack/react-router'
-import TeamsList from './-components/TeamsList'
+import TeamsList from './-components/TeamsList/TeamsList'
 
 export const Route = createFileRoute('/_layout/teams/')({
   component: Teams,
+  pendingComponent: () => <Loading page="teamsList" />,
 })
 
 function Teams() {
