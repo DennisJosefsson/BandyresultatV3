a39803ecdd5a7ccb6f78d59e6cb123dcd501b96c
DennisJosefsson <dennis.josefsson@gmail.com>
2025-12-12 11:26:35 +0100

    Removed duplicates

--- src/db/schemas/county.schema.ts
+++
@@ -1,6 +0,0 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
-
-export const county = pgTable('county', {
-  countyId: integer('county_id').primaryKey().notNull(),
-  name: varchar().notNull(),
-})
