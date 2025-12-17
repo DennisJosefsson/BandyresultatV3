9f003241e5d77932fc3ca4751a2c13f5e3fa1d09
DennisJosefsson <dennis.josefsson@gmail.com>
2025-12-17 10:50:23 +0100

    Team season routes

--- src/routes/_layout/team/$teamId/$seasonId.tsx
+++ src/routes/_layout/team/$teamId/$seasonId.tsx
@@ -1,9 +1,46 @@
-import { createFileRoute } from '@tanstack/react-router'
+import SimpleErrorComponent from '@/components/ErrorComponents/SimpleErrorComponent'
+import { zd } from '@/lib/utils/zod'
+import { CatchBoundary, createFileRoute } from '@tanstack/react-router'
+
+import SingleTeamSeason from '../-components/SeasonComponents/SingleTeamSeason'
+import { getSingleTeamSeason } from '../-functions/getSingleTeamSeason'
 
 export const Route = createFileRoute('/_layout/team/$teamId/$seasonId')({
-  component: RouteComponent,
+  params: {
+    parse: (params) => ({
+      seasonId: zd.number().int().parse(Number(params.seasonId)),
+    }),
+    stringify: ({ seasonId }) => ({ seasonId: `${seasonId}` }),
+  },
+  loader: async ({ params: { teamId, seasonId } }) => {
+    const data = await getSingleTeamSeason({
+      data: { teamId: teamId, seasonId },
+    })
+    if (!data) {
+      throw new Error('Något gick fel.')
+    }
+
+    return data
+  },
+  component: Season,
 })
 
-function RouteComponent() {
-  return <div>Hello "/_layout/team/$teamId/$seasonId"!</div>
+function Season() {
+  return (
+    <div>
+      <CatchBoundary
+        getResetKey={() => 'reset'}
+        onCatch={(error) => {
+          console.error(error)
+        }}
+        errorComponent={({ error, reset }) => (
+          <SimpleErrorComponent id="singleteam" error={error} reset={reset} />
+        )}
+      >
+        <div className="font-inter text-foreground mt-2 flex min-h-screen w-full flex-col">
+          <SingleTeamSeason />
+        </div>
+      </CatchBoundary>
+    </div>
+  )
 }
--- src/routes/_layout/team/$teamId/seasons.tsx
+++ src/routes/_layout/team/$teamId/seasons.tsx
@@ -1,9 +1,90 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+import { CardContent } from '@/components/ui/card'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { useState } from 'react'
+import { getTeamSeasons } from '../-functions/teamSeasons'
 
 export const Route = createFileRoute('/_layout/team/$teamId/seasons')({
-  component: RouteComponent,
+  loader: async ({ params }) => {
+    const data = await getTeamSeasons({ data: params.teamId })
+    if (!data) {
+      throw new Error('Något gick fel.')
+    }
+    return data
+  },
+  component: Seasons,
 })
 
-function RouteComponent() {
-  return <div>Hello "/_layout/team/$teamId/seasons"!</div>
+function Seasons() {
+  const [open, setOpen] = useState('seasons')
+  const seasons = Route.useLoaderData({ select: (data) => data.seasons })
+  const rest = Route.useLoaderData({ select: (data) => data.rest })
+
+  return (
+    <CardContent className="p-1 md:p-2">
+      <Accordion type="single" collapsible value={open} onValueChange={setOpen}>
+        <AccordionItem
+          value="seasons"
+          className="bg-background mb-2 rounded-md p-2 shadow-md"
+        >
+          <AccordionTrigger className="text-sm md:text-base">
+            Senaste säsongerna
+          </AccordionTrigger>
+          <AccordionContent>
+            <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 xl:grid-cols-8">
+              {seasons.map((season) => {
+                return (
+                  <Link
+                    key={season.seasonId}
+                    from="/team/$teamId/seasons"
+                    to="/team/$teamId/$seasonId"
+                    params={{ seasonId: season.seasonId }}
+                    search={(prev) => ({ ...prev })}
+                  >
+                    <div className="bg-muted dark:bg-muted/50 flex items-center justify-center rounded p-2 text-[10px] font-semibold sm:text-sm md:text-base">
+                      {season.year}
+                    </div>
+                  </Link>
+                )
+              })}
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+        {rest.length > 0 ? (
+          <AccordionItem
+            value="rest"
+            className="bg-background mb-2 rounded-md p-2 shadow-md"
+          >
+            <AccordionTrigger className="text-sm md:text-base">
+              Övriga
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 xl:grid-cols-8">
+                {rest.map((season) => {
+                  return (
+                    <Link
+                      key={season.seasonId}
+                      from="/team/$teamId/seasons"
+                      to="/team/$teamId/$seasonId"
+                      params={{ seasonId: season.seasonId }}
+                      search={(prev) => ({ ...prev })}
+                    >
+                      <div className="bg-muted dark:bg-muted/50 flex items-center justify-center rounded p-2 text-[10px] font-semibold sm:text-sm md:text-base">
+                        {season.year}
+                      </div>
+                    </Link>
+                  )
+                })}
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        ) : null}
+      </Accordion>
+    </CardContent>
+  )
 }
