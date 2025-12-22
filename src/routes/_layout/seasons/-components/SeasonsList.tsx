7c6f62ea0cff82b637ee4fbfa79d2429e86b20fb
DennisJosefsson <dennis.josefsson@gmail.com>
2025-12-22 16:17:51 +0100

    Added group default to paginated seasons, updated seasonslist

--- src/routes/_layout/seasons/-components/SeasonsList.tsx
+++ src/routes/_layout/seasons/-components/SeasonsList.tsx
@@ -1,47 +1,53 @@
-import { useLoaderData } from '@tanstack/react-router'
+import { Link, useLoaderData, useSearch } from '@tanstack/react-router'
 
 const SeasonsList = () => {
-  //   const women = useSearch({
-  //     from: '/_layout',
-  //     select: (search) => search.women,
-  //   })
+  const women = useSearch({
+    from: '/_layout',
+    select: (search) => search.women,
+  })
   const seasons = useLoaderData({
     from: '/_layout/seasons/',
     select: (s) => s.seasons,
   })
   return (
-    <div className="grid justify-between grid-cols-1 pt-2 gap-x-8 gap-y-2 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+    <div className="grid grid-cols-1 justify-between gap-x-8 gap-y-2 pt-2 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
       {seasons.map((season) => {
-        // const seasonYear =
-        //   parseInt(season.year.split('/')[1]) >= 1964
-        //     ? parseInt(season.year.split('/')[1])
-        //     : parseInt(season.year)
+        const year =
+          parseInt(season.year.split('/')[1]) >= 1964
+            ? parseInt(season.year.split('/')[1])
+            : parseInt(season.year)
 
         return (
           <div
             key={season.seasonId}
-            className="flex flex-row items-center justify-between px-2 py-1 text-sm bg-muted dark:bg-muted/50 lg:text-base 2xl:text-lg"
+            className="bg-muted dark:bg-muted/50 flex flex-row items-center justify-between px-2 py-1 text-sm lg:text-base 2xl:text-lg"
           >
-            <div className="font-semibold w-28">{season.year}</div>
-            <div className="px-2 py-1 text-center rounded-md xl:p-0">
-              {/* <Link
-                to="/season/$seasonId/tables/$table"
-                params={{ seasonId: seasonYear, table: 'all' }}
+            <div className="w-28 font-semibold">{season.year}</div>
+            <div className="rounded-md px-2 py-1 text-center xl:p-0">
+              <Link
+                to="/season/$year/{-$group}"
+                params={{
+                  year: year,
+                  group: women ? season.womensGroup : season.mensGroup,
+                }}
                 search={{ women }}
-                className="font-medium tabular-nums hover:font-bold hover:text-primary lg:font-normal"
-              > */}
-              Tabeller
-              {/* </Link> */}
+                className="hover:text-primary font-medium tabular-nums hover:font-bold lg:font-normal"
+              >
+                Tabeller
+              </Link>
             </div>
-            <div className="px-2 py-1 text-center rounded-md xl:p-0">
-              {/* <Link
-                to="/season/$seasonId/games"
-                params={{ seasonId: seasonYear }}
+            <div className="rounded-md px-2 py-1 text-center xl:p-0">
+              <Link
+                to="/season/$year/{-$group}/games"
+                params={{
+                  year: year,
+                  group: women ? season.womensGroup : season.mensGroup,
+                }}
                 search={{ women }}
-                className="font-medium hover:font-bold hover:text-primary lg:font-normal"
-              > */}
-              Matcher
-              {/* </Link> */}
+                className="hover:text-primary font-medium hover:font-bold lg:font-normal"
+              >
+                Matcher
+              </Link>
             </div>
           </div>
         )
--- src/routes/_layout/seasons/-functions/getPaginatedSeasons.ts
+++ src/routes/_layout/seasons/-functions/getPaginatedSeasons.ts
@@ -1,8 +1,8 @@
 import { db } from '@/db'
-import { seasons } from '@/db/schema'
+import { seasons, series } from '@/db/schema'
 import { createServerFn } from '@tanstack/react-start'
 import { zodValidator } from '@tanstack/zod-adapter'
-import { eq } from 'drizzle-orm'
+import { and, asc, eq, sql } from 'drizzle-orm'
 import { z } from 'zod'
 
 export const page = z.number().catch(1)
@@ -12,6 +12,30 @@ export const getPaginatedSeasons = createServerFn({ method: 'GET' })
   .handler(async ({ data }) => {
     const count = await db.$count(seasons, eq(seasons.women, false))
 
+    const ranked = db.$with('ranked').as(
+      db
+        .select({
+          seasonId: series.seasonId,
+          year: seasons.year,
+          women: seasons.women,
+          group: series.serieGroupCode,
+          rankedGroup:
+            sql<number>`rank() over (partition by series.season_id, seasons.women order by series.serie_group_code)`
+              .mapWith(Number)
+              .as('ranked_group'),
+        })
+        .from(series)
+        .leftJoin(seasons, eq(seasons.seasonId, series.seasonId))
+        .where(and(eq(series.level, 1), eq(series.serieCategory, 'regular'))),
+    )
+
+    const groups = await db
+      .with(ranked)
+      .select()
+      .from(ranked)
+      .where(eq(ranked.rankedGroup, 1))
+      .orderBy(asc(ranked.year), asc(ranked.women))
+
     const pagSeasons = await db.query.seasons.findMany({
       columns: { seasonId: true, year: true },
       where: (seasons, { eq }) => eq(seasons.women, false),
@@ -19,5 +43,20 @@ export const getPaginatedSeasons = createServerFn({ method: 'GET' })
       limit: 12,
       orderBy: (seasons, { desc }) => desc(seasons.seasonId),
     })
-    return { count, seasons: pagSeasons }
+
+    const combinedSeasons = pagSeasons.map((s) => {
+      const mensGroup = groups.find(
+        (g) => g.year === s.year && g.women === false,
+      )
+      const womensGroup = groups.find(
+        (g) => g.year === s.year && g.women === true,
+      )
+      return {
+        ...s,
+        mensGroup: mensGroup?.group ?? undefined,
+        womensGroup: womensGroup?.group ?? undefined,
+      }
+    })
+
+    return { count, seasons: combinedSeasons }
   })
