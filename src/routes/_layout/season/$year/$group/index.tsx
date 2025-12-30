406433aeee68908bfe541ff90e47a09a256d53bc
DennisJosefsson <dennis.josefsson@gmail.com>
2025-12-30 14:09:48 +0100

    Validating group on index route

--- src/routes/_layout/season/$year/$group/index.tsx
+++ src/routes/_layout/season/$year/$group/index.tsx
@@ -1,18 +1,41 @@
 import {
   createFileRoute,
+  notFound,
   Outlet,
   useChildMatches,
 } from '@tanstack/react-router'
+import { validateGroup } from '../-functions/validateGroup'
 
 export const Route = createFileRoute('/_layout/season/$year/$group/')({
+  loaderDeps: ({ search: { women } }) => ({ women }),
+  loader: async ({ params: { year, group }, deps: { women } }) => {
+    const result = await validateGroup({ data: { year, group, women } })
+    if (result.status === 404) throw notFound({ data: 'Serien finns inte.' })
+    return result
+  },
   component: RouteComponent,
+  notFoundComponent(props) {
+    if (props.data && typeof props.data === 'string') {
+      return (
+        <div className="mt-4 flex flex-row justify-center">
+          <p>{props.data}</p>
+        </div>
+      )
+    }
+    return (
+      <div className="mt-4 flex flex-row justify-center">
+        <p>Något gick tyvärr fel.</p>
+      </div>
+    )
+  },
 })
 
 function RouteComponent() {
   const matches = useChildMatches()
+
   if (matches.length === 0) {
     return (
-      <div>
+      <div className="mt-4 flex flex-row justify-center">
         <p>Välj någon av alternativen i menyn.</p>
       </div>
     )
---
+++ src/routes/_layout/season/$year/-functions/validateGroup.ts
@@ -0,0 +1,48 @@
+import { db } from '@/db'
+import { seasons, series } from '@/db/schema'
+import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
+import { seasonIdCheck } from '@/lib/utils/utils'
+import { zd } from '@/lib/utils/zod'
+import { createServerFn } from '@tanstack/react-start'
+import { zodValidator } from '@tanstack/zod-adapter'
+import { and, eq, getTableColumns } from 'drizzle-orm'
+
+type ValidateGroupReturn =
+  | {
+      status: 404
+    }
+  | { status: 200 }
+
+export const validateGroup = createServerFn({ method: 'GET' })
+  .middleware([errorMiddleware])
+  .inputValidator(
+    zodValidator(
+      zd.object({ group: zd.string(), year: zd.int(), women: zd.boolean() }),
+    ),
+  )
+  .handler(
+    async ({ data: { group, year, women } }): Promise<ValidateGroupReturn> => {
+      const seasonYear = seasonIdCheck.parse(year)
+      if (!seasonYear) throw new Error('Säsongen finns inte.')
+      const serie = await db
+        .select({
+          ...getTableColumns(series),
+        })
+        .from(series)
+        .leftJoin(seasons, eq(seasons.seasonId, series.seasonId))
+        .where(
+          and(
+            eq(series.serieGroupCode, group),
+            eq(seasons.women, women),
+            eq(seasons.year, seasonYear),
+          ),
+        )
+        .then((res) => {
+          if (res.length > 0) return res[0]
+          else return undefined
+        })
+
+      if (!serie) return { status: 404 }
+      return { status: 200 }
+    },
+  )
