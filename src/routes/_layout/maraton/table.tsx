f1e0e9ec40db9528586ee15c7ed0ac3ec5419a3f
DennisJosefsson <dennis.josefsson@gmail.com>
2026-01-14 13:22:25 +0100

    Maraton routes

--- src/routes/_layout/maraton/table.tsx
+++ src/routes/_layout/maraton/table.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/_layout/maraton/')({
+export const Route = createFileRoute('/_layout/maraton/table')({
   component: RouteComponent,
 })
 
