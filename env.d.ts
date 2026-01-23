9a825b51320f20f177c3e4c1a3f238104239a4b3
DennisJosefsson <dennis.josefsson@gmail.com>
2026-01-23 14:47:38 +0100

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,9 @@
 interface ImportMetaEnv {
   // Client-side environment variables
   readonly VITE_CLERK_PUBLISHABLE_KEY: string
+  readonly VITE_SITE_LOCALHOST_URL: string
+  readonly VITE_SITE_DEV_URL: string
+  readonly VITE_SITE_PROD_URL: string
   //   readonly VITE_API_URL: string
   //   readonly VITE_AUTH0_DOMAIN: string
   //   readonly VITE_AUTH0_CLIENT_ID: string
