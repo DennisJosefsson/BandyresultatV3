9f400dad5e67c1239de5b6fc226d8099f2e8395b
DennisJosefsson <dennis.josefsson@gmail.com>
2026-01-23 14:50:42 +0100

    Added getBaseUrl function

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -20,7 +20,26 @@ export const seasonIdCheck = zd.coerce
     }
   })
 
-export const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://bandyresultat.se'
+type HostName = 'localhost' | 'dev.bandyresultat.se' | 'bandyresultat.se'
+
+export const getBaseUrl = () => {
+  let baseUrl: string
+
+  const hostname = location.hostname as HostName
+
+  switch (hostname) {
+    case 'localhost':
+      baseUrl = import.meta.env.VITE_SITE_LOCALHOST_URL
+      break
+    case 'dev.bandyresultat.se':
+      baseUrl = import.meta.env.VITE_SITE_DEV_URL
+      break
+    case 'bandyresultat.se':
+      baseUrl = import.meta.env.VITE_SITE_PROD_URL
+      break
+    default:
+      baseUrl = import.meta.env.VITE_SITE_LOCALHOST_URL
+  }
+
+  return baseUrl
+}
