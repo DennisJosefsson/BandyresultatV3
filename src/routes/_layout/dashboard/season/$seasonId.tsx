ae001585071ac8870363a3526e051a8277e11f94
DennisJosefsson <dennis.josefsson@gmail.com>
2026-01-28 12:40:04 +0100

    Parse seasonId as number

--- src/routes/_layout/dashboard/season/$seasonId.tsx
+++ src/routes/_layout/dashboard/season/$seasonId.tsx
@@ -1,6 +1,13 @@
+import { zd } from '@/lib/utils/zod'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_layout/dashboard/season/$seasonId')({
+  params: {
+    parse: (params) => ({
+      seasonId: zd.number().int().parse(Number(params.seasonId)),
+    }),
+    stringify: ({ seasonId }) => ({ seasonId: `${seasonId}` }),
+  },
   component: RouteComponent,
 })
 
