3a6f123c541b031c97eb279e52846e260fc55e7c
DennisJosefsson <dennis.josefsson@gmail.com>
2026-02-24 18:07:53 +0100

    Added head meta data for teams list

--- src/routes/_layout/teams/index.tsx
+++ src/routes/_layout/teams/index.tsx
@@ -6,6 +6,38 @@ import TeamsList from './-components/TeamsList/TeamsList'
 export const Route = createFileRoute('/_layout/teams/')({
   component: Teams,
   pendingComponent: () => <Loading page="teamsList" />,
+  head: () => ({
+    meta: [
+      {
+        title: 'Bandyresultat - Laglista',
+      },
+      {
+        name: 'description',
+        content: 'Bandyresultat - Lista över svenska bandylag',
+      },
+      {
+        property: 'og:description',
+        content: 'Bandyresultat - Lista över svenska bandylag',
+      },
+      {
+        property: 'og:title',
+        content: 'Bandyresultat - Laglista',
+      },
+      {
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        property: 'og:url',
+        content: 'https://bandyresultat.se/teams',
+      },
+      {
+        property: 'og:image',
+        content:
+          'https://github.com/DennisJosefsson/WebsiteImages/blob/main/bandyresultat.jpg?raw=true',
+      },
+    ],
+  }),
 })
 
 function Teams() {
