5f4990dee2f46ce339703944868e0e6894d8ca5c
DennisJosefsson <dennis.josefsson@gmail.com>
2026-03-05 09:20:26 +0100

    Radix button to base-ui button

--- src/components/ErrorComponents/SimpleErrorComponent.tsx
+++ src/components/ErrorComponents/SimpleErrorComponent.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/base/ui/button'
 
 type ErrorComponentProps = {
   id: string
@@ -6,14 +6,19 @@ type ErrorComponentProps = {
   reset: () => void
 }
 
-const SimpleErrorComponent = ({ id, error, reset }: ErrorComponentProps) => {
+const SimpleErrorComponent = ({
+  id,
+  error,
+  reset,
+}: ErrorComponentProps) => {
   return (
     <div className="font-inter text-foreground mx-auto mt-10 flex items-center justify-center">
       <div className="mx-2 max-w-3xl">
         <p className="mb-4 text-center text-[10px] md:text-sm">
-          Om du ser det här så har något gått jättefel. Maila gärna
-          dennis@bandyresultat.se och ta med det som står här nedanför i ditt
-          meddelande. Tack på förhand.
+          Om du ser det här så har något gått jättefel.
+          Maila gärna dennis@bandyresultat.se och ta med det
+          som står här nedanför i ditt meddelande. Tack på
+          förhand.
         </p>
         <p className="text-center text-[10px] md:text-sm">
           id: {id}-{error.message}
