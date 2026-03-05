b2a9af82806f803db19d18cd046462a50b578294
DennisJosefsson <dennis.josefsson@gmail.com>
2026-03-05 10:11:41 +0100

    radix logout button to base-ui

--- src/routes/_layout/logout/index.tsx
+++ src/routes/_layout/logout/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/base/ui/button'
 import { SignOutButton } from '@clerk/clerk-react'
 import { createFileRoute } from '@tanstack/react-router'
 
@@ -10,7 +10,7 @@ function RouteComponent() {
   return (
     <div className="flex flex-row items-center justify-center mt-20">
       <SignOutButton>
-        <Button variant="ghost">
+        <Button variant="outline">
           <span className="text-base font-semibold">
             Logga ut
           </span>
