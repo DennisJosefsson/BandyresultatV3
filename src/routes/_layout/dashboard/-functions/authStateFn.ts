1b80f1d2b3ededdefd048af21dbfdf3c57efa77b
DennisJosefsson <dennis.josefsson@gmail.com>
2026-03-24 09:55:05 +0100

    Changed from throwing to returning

--- src/routes/_layout/dashboard/-functions/authStateFn.ts
+++ src/routes/_layout/dashboard/-functions/authStateFn.ts
@@ -1,17 +1,11 @@
 import { auth } from '@clerk/tanstack-react-start/server'
-import { redirect } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
 
-export const authStateFn = createServerFn({ method: 'GET' }).handler(
-  async () => {
-    const { orgRole } = await auth()
-    const isAdmin = orgRole === 'org:admin'
+export const authStateFn = createServerFn({
+  method: 'GET',
+}).handler(async () => {
+  const { orgRole } = await auth()
+  const isAdmin = orgRole === 'org:admin'
 
-    if (!isAdmin) {
-      throw redirect({
-        to: '/unauthorized',
-        search: { women: false },
-      })
-    }
-  },
-)
+  return { isAdmin }
+})
