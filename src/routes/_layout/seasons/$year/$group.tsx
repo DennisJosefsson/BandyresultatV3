248421fcadd70f4d75d82b340e877e0b3d925cc9
DennisJosefsson <dennis.josefsson@gmail.com>
2026-03-25 17:24:27 +0100

    Added error boundary to group route

--- src/routes/_layout/seasons/$year/$group.tsx
+++ src/routes/_layout/seasons/$year/$group.tsx
@@ -1,25 +1,48 @@
 import {
+  CatchBoundary,
   Outlet,
   createFileRoute,
-  useChildMatches,
 } from '@tanstack/react-router'
 
+import SimpleErrorComponent from '@/components/ErrorComponents/SimpleErrorComponent'
 import { validateGroup } from './-functions/validateGroup'
 
-export const Route = createFileRoute('/_layout/seasons/$year/$group')({
+export const Route = createFileRoute(
+  '/_layout/seasons/$year/$group',
+)({
   loaderDeps: ({ search: { women } }) => ({ women }),
-  loader: async ({ params: { year, group }, deps: { women } }) => {
-    const data = await validateGroup({ data: { year, group, women } })
+  loader: async ({
+    params: { year, group },
+    deps: { women },
+  }) => {
+    const data = await validateGroup({
+      data: { year, group, women },
+    })
 
     return data
   },
-  staticData: { breadcrumb: (match) => match.loaderData.breadCrumb },
+  staticData: {
+    breadcrumb: (match) => match.loaderData.breadCrumb,
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const childMatches = useChildMatches()
-  if (childMatches.length === 0) return <div>Lite info</div>
-
-  return <Outlet />
+  return (
+    <CatchBoundary
+      getResetKey={() => 'reset'}
+      onCatch={(error) => {
+        console.error(error)
+      }}
+      errorComponent={({ error, reset }) => (
+        <SimpleErrorComponent
+          id="groupRoute"
+          error={error}
+          reset={reset}
+        />
+      )}
+    >
+      <Outlet />
+    </CatchBoundary>
+  )
 }
