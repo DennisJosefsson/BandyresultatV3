640d4c708e674e73b3d6890d2c49a2ded3f4a123
DennisJosefsson <dennis.josefsson@gmail.com>
2026-03-31 08:42:46 +0200

    Added prod db config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import type { PoolConfig } from 'pg'
 import { Pool } from 'pg'
 
 import * as schema from './schema.ts'
@@ -11,7 +12,21 @@ const dbConnectionString =
     ? process.env.DB_DEVELOPMENT_URL
     : process.env.DB_PRODUCTION_URL
 
-const pool = new Pool({
-  connectionString: dbConnectionString,
-})
+const prodConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { ca: process.env.PEM },
+} satisfies PoolConfig
+
+const dbConfig =
+  process.env.NODE_ENV === 'development'
+    ? {
+        connectionString: dbConnectionString,
+      }
+    : prodConfig
+
+const pool = new Pool(dbConfig)
 export const db = drizzle(pool, { schema })
