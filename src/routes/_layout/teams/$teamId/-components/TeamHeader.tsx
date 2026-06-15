e43817f056866140126e2584716013a8ff2cbee1
DennisJosefsson <dennis.josefsson@gmail.com>
2026-06-15 18:09:13 +0200

    Added team logo to teamheader

--- src/routes/_layout/teams/$teamId/-components/TeamHeader.tsx
+++ src/routes/_layout/teams/$teamId/-components/TeamHeader.tsx
@@ -1,6 +1,6 @@
-import { Link, getRouteApi } from '@tanstack/react-router'
-import { useFavTeam } from '@/lib/contexts/favTeamsContext'
 import { Button } from '@/components/base/ui/button'
+import { useFavTeam } from '@/lib/contexts/favTeamsContext'
+import { Link, getRouteApi } from '@tanstack/react-router'
 
 const route = getRouteApi('/_layout/teams/$teamId')
 
@@ -35,9 +35,12 @@ const TeamHeader = () => {
   return (
     <div className="mb-4">
       <div className="flex flex-row items-center justify-between">
-        <div className="flex flex-row flex-wrap gap-2 text-xs sm:text-sm md:text-base xl:text-lg">
-          <span>{data.team.name}</span>
-          <div className="flex flex-row gap-2">
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-row flex-wrap gap-2 text-sm sm:text-nd md:text-lg xl:text-2xl tracking-wide xs:tracking-widest">
+            <span>{data.team.name}</span>
+          </div>
+
+          <div className="flex flex-row gap-2 items-center text-xs sm:text-sm md:text-base xl:text-lg">
             <span>[ </span>
             <Link
               to="/teams/$teamId"
@@ -59,25 +62,31 @@ const TeamHeader = () => {
             </Link>
             <span> ]</span>
           </div>
+          <div>
+            {favTeams.includes(teamId) && (
+              <Button
+                onClick={remove}
+                size="responsive"
+              >
+                Ta bort favorit
+              </Button>
+            )}
+            {!favTeams.includes(teamId) && (
+              <Button
+                onClick={add}
+                size="responsive"
+              >
+                Favoritlag
+              </Button>
+            )}
+          </div>
         </div>
 
-        <div className="flex flex-row items-center gap-1">
-          {/* {origin ? (
-            <Button onClick={goBack} size='responsive'>
-              Tillbaka
-            </Button>
-          ) : null} */}
-
-          {favTeams.includes(teamId) && (
-            <Button onClick={remove} size="responsive">
-              Ta bort favorit
-            </Button>
-          )}
-          {!favTeams.includes(teamId) && (
-            <Button onClick={add} size="responsive">
-              Favoritlag
-            </Button>
-          )}
+        <div className="bg-accent rounded-full border border-accent-foreground overflow-hidden w-8 xs:w-16 md:w-24 lg:w-32">
+          <img
+            className="w-8 xs:w-16 md:w-24 lg:w-32 object-scale-down"
+            src={`/logos/teams/128/${teamId}_128x128.png`}
+          ></img>
         </div>
       </div>
     </div>
