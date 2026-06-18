05d16077a09d803452bca3a26e0460188f9b456a
DennisJosefsson <dennis.josefsson@gmail.com>
2026-06-18 18:36:12 +0200

    Execution timing

--- src/routes/_layout/teams/$teamId/-functions/getSingleTeam.ts
+++ src/routes/_layout/teams/$teamId/-functions/getSingleTeam.ts
@@ -1,5 +1,7 @@
-import { zodValidator } from '@tanstack/zod-adapter'
-import { createServerFn } from '@tanstack/react-start'
+import { catchError } from '@/lib/middlewares/errors/catchError'
+import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
+import type { Meta } from '@/lib/types/meta'
+import type { SingleTeamTables } from '@/lib/types/table'
 import type {
   FiveSeason,
   SingleTeam,
@@ -7,16 +9,18 @@ import type {
   TeamStatItem,
   TeamStreak,
 } from '@/lib/types/team'
-import type { SingleTeamTables } from '@/lib/types/table'
-import type { Meta } from '@/lib/types/meta'
 import { zd } from '@/lib/utils/zod'
-import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
-import { catchError } from '@/lib/middlewares/errors/catchError'
-import { getAllTeamsSeasons, getStrings, getTeam } from './singleTeamQueries'
-import { getTables } from './getTables'
-import { getStreaks } from './getStreaks'
-import { getStats } from './getStats'
+import { createServerFn } from '@tanstack/react-start'
+import { zodValidator } from '@tanstack/zod-adapter'
 import { getLastFiveSeasons } from './getLastFiveSeasons'
+import { getStats } from './getStats'
+import { getStreaks } from './getStreaks'
+import { getTables } from './getTables'
+import {
+  getAllTeamsSeasons,
+  getStrings,
+  getTeam,
+} from './singleTeamQueries'
 
 type TeamReturn =
   | {
@@ -71,68 +75,104 @@ export const getSingleTeam = createServerFn({
       zd
         .number('Lag-id måste vara en siffra.')
         .int('Lag-id måste vara ett heltal.')
-        .positive('Lag-id får ej vara ett minustal eller noll.'),
+        .positive(
+          'Lag-id får ej vara ett minustal eller noll.',
+        ),
     ),
   )
   .middleware([errorMiddleware])
-  .handler(async ({ data: teamId }): Promise<TeamReturn> => {
-    try {
-      const team = await getTeam(teamId)
-      let breadCrumb = `Lag`
-      let title = `Bandyresultat - Laget finns ej`
-      let url = `https://bandyresultat.se/teams`
-      let description = `Laget finns ej.`
-      if (!team || team.teamId === 176) {
-        return {
-          status: 404,
-          meta: { title, url, description },
-          breadCrumb,
-          message: 'Det laget finns inte.',
+  .handler(
+    async ({ data: teamId }): Promise<TeamReturn> => {
+      try {
+        const start = performance.now()
+        const team = await getTeam(teamId)
+        let breadCrumb = `Lag`
+        let title = `Bandyresultat - Laget finns ej`
+        let url = `https://bandyresultat.se/teams`
+        let description = `Laget finns ej.`
+        if (!team || team.teamId === 176) {
+          return {
+            status: 404,
+            meta: { title, url, description },
+            breadCrumb,
+            message: 'Det laget finns inte.',
+          }
         }
+        const teamCheckTime = performance.now()
+
+        const allSeasons = await getAllTeamsSeasons(teamId)
+
+        const allSeasonsTime = performance.now()
+
+        const strings = await getStrings({
+          team,
+          allSeasons,
+        })
+
+        const stringsTime = performance.now()
+
+        const tables = await getTables({
+          teamId,
+          seasonIdArray: allSeasons.rows.map(
+            (season) => season.seasonId,
+          ),
+        })
+
+        const tablesTime = performance.now()
+
+        const streaks = await getStreaks({
+          teamId,
+        })
+
+        const streaksTime = performance.now()
+
+        const stats = await getStats({ teamId })
+
+        const statsTime = performance.now()
+
+        const fiveSeasons = await getLastFiveSeasons({
+          teamId,
+        })
+
+        const fiveSeasonsTime = performance.now()
+
+        breadCrumb = `${team.name}`
+        title = `Bandyresultat - ${team.name} - ${team.women === true ? 'Damer' : 'Herrar'}`
+        url = `https://bandyresultat.se/teams/${team.teamId}?women=${team.women}`
+        description = `Information om ${team.name} ${team.women ? 'damer' : 'herrar'}`
+
+        const meta = {
+          title,
+          url,
+          description,
+        }
+
+        const end = performance.now()
+        const time = end - start
+        console.log('Execution times: ', {
+          teamCheckTime: teamCheckTime - start,
+          allSeasonsTime: allSeasonsTime - teamCheckTime,
+          stringsTime: stringsTime - allSeasonsTime,
+          tablesTime: tablesTime - stringsTime,
+          streaksTime: streaksTime - tablesTime,
+          statsTime: statsTime - streaksTime,
+          fiveSeasonsTime: fiveSeasonsTime - statsTime,
+          executionTime: time,
+        })
+
+        return {
+          status: 200,
+          team,
+          tables,
+          strings,
+          streaks,
+          stats,
+          fiveSeasons,
+          breadCrumb,
+          meta,
+        }
+      } catch (error) {
+        catchError(error)
       }
-
-      const allSeasons = await getAllTeamsSeasons(teamId)
-      const strings = await getStrings({
-        team,
-        allSeasons,
-      })
-      const tables = await getTables({
-        teamId,
-        seasonIdArray: allSeasons.rows.map((season) => season.seasonId),
-      })
-
-      const streaks = await getStreaks({
-        teamId,
-      })
-
-      const stats = await getStats({ teamId })
-
-      const fiveSeasons = await getLastFiveSeasons({
-        teamId,
-      })
-
-      breadCrumb = `${team.name}`
-      title = `Bandyresultat - ${team.name} - ${team.women === true ? 'Damer' : 'Herrar'}`
-      url = `https://bandyresultat.se/teams/${team.teamId}?women=${team.women}`
-      description = `Information om ${team.name} ${team.women ? 'damer' : 'herrar'}`
-      const meta = {
-        title,
-        url,
-        description,
-      }
-
-      return {
-        status: 200,
-        team,
-        tables,
-        strings,
-        streaks,
-        stats,
-        fiveSeasons,
-        breadCrumb,
-        meta,
-      }
-    } catch (error) {
-      catchError(error)
-    }
-  })
+    },
+  )
