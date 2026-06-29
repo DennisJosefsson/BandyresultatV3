07b5a1b7ebcb2928361caf8c33d55e6a15a7d330
DennisJosefsson <dennis.josefsson@gmail.com>
2026-06-29 18:27:49 +0200

    Redesign single team stats and games components

--- src/routes/_layout/teams/$teamId/-components/GameStats/GameStatComponent.tsx
+++ src/routes/_layout/teams/$teamId/-components/GameStats/GameStatComponent.tsx
@@ -1,52 +1,51 @@
-import type { ReactNode } from 'react'
-import type { TeamStatItem } from '@/lib/types/team'
 import Date from '@/components/Common/Date'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/base/ui/card'
+import type { TeamStatItem } from '@/lib/types/team'
+import type { ReactNode } from 'react'
 
-const GameStatComponent = ({ children }: { children: ReactNode }) => {
-  return (
-    <Card className="mb-2" size="sm">
-      {children}
-    </Card>
-  )
+const GameStatComponent = ({
+  children,
+}: {
+  children: ReactNode
+}) => {
+  return <div className="mb-2">{children}</div>
 }
 
 function Title({ children }: { children: ReactNode }) {
   return (
-    <CardHeader>
-      <CardTitle className="xs:group-data-[size=sm]/card:text-xs group-data-[size=sm]/card:text-[10px] md:group-data-[size=sm]/card:text-sm">
-        {children}
-      </CardTitle>
-    </CardHeader>
+    <div className="xs:text-xs text-[8px] md:text-sm mb-1">
+      {children}
+    </div>
   )
 }
 
-function Content({ statArray }: { statArray: Array<TeamStatItem> }) {
+function Content({
+  statArray,
+}: {
+  statArray: Array<TeamStatItem>
+}) {
   if (!statArray || statArray.length === 0) return null
 
   return (
-    <CardContent>
-      <div>
-        {statArray.map((stat, index) => {
-          return (
-            <div
-              key={`${stat.gameId}-${index}`}
-              className="bg-muted-foreground/20 xs:p-2 mb-1 flex flex-col gap-1 rounded p-1 text-[10px] lg:text-sm"
-            >
-              <div className="flex flex-row justify-between">
-                <span>
-                  {stat.homeTeam}-{stat.awayTeam}
-                </span>
-                <span>{stat.result}</span>
-              </div>
-              <div>
-                <Date>{stat.date}</Date>
-              </div>
+    <div>
+      {statArray.map((stat, index) => {
+        return (
+          <div
+            key={`${stat.gameId}-${index}`}
+            className="bg-muted-foreground/20 xs:p-2 mb-1 flex flex-col gap-1 rounded p-1 xs:text-xs text-[8px] lg:text-sm max-w-md"
+          >
+            <div className="flex flex-row justify-between">
+              <span>
+                {stat.homeTeam}-{stat.awayTeam}
+              </span>
+              <span>{stat.result}</span>
             </div>
-          )
-        })}
-      </div>
-    </CardContent>
+            <div>
+              <Date>{stat.date}</Date>
+            </div>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
--- src/routes/_layout/teams/$teamId/-components/SeasonComponents/Games/GamesListItem.tsx
+++ src/routes/_layout/teams/$teamId/-components/SeasonComponents/Games/GamesListItem.tsx
@@ -1,9 +1,14 @@
-import { getRouteApi } from '@tanstack/react-router'
+import {
+  TableCell,
+  TableRow,
+} from '@/components/base/ui/table'
 import type { Game } from '@/lib/types/game'
 import { cn } from '@/lib/utils/utils'
-import { TableCell, TableRow } from '@/components/base/ui/table'
+import { getRouteApi } from '@tanstack/react-router'
 
-const route = getRouteApi('/_layout/teams/$teamId/$seasonId')
+const route = getRouteApi(
+  '/_layout/teams/$teamId/seasons/$seasonId/',
+)
 
 type GamesListItemProps = {
   game: Game
@@ -18,7 +23,9 @@ const GamesListItem = ({ game }: GamesListItemProps) => {
       <TableCell
         className={cn(
           'w-12 xs:w-18 p-0 py-1 truncate',
-          data.team.casualName === game.home.casualName ? 'text-primary font-bold' : null,
+          data.team.casualName === game.home.casualName
+            ? 'text-primary font-bold'
+            : null,
         )}
       >
         {game.home.casualName}
@@ -27,22 +34,32 @@ const GamesListItem = ({ game }: GamesListItemProps) => {
       <TableCell
         className={cn(
           'w-12 xs:w-18 p-0 py-1 truncate',
-          data.team.casualName === game.away.casualName ? 'text-primary font-bold' : null,
+          data.team.casualName === game.away.casualName
+            ? 'text-primary font-bold'
+            : null,
         )}
       >
         {game.away.casualName}
       </TableCell>
 
       <TableCell className="xs:w-18 w-12 p-0 py-1 tabular-nums">
-        {game.otResult ? `${game.otResult} (${game.result})` : game.result}
+        {game.otResult
+          ? `${game.otResult} (${game.result})`
+          : game.result}
       </TableCell>
 
       <TableCell className="w-8 p-0 py-1 tabular-nums">
-        {game.halftimeResult ? `(${game.halftimeResult})` : null}
+        {game.halftimeResult
+          ? `(${game.halftimeResult})`
+          : null}
       </TableCell>
 
       <TableCell className="w-6 p-0 py-1 text-center">
-        {game.penalties ? 'S' : game.extraTime ? 'ÖT' : null}
+        {game.penalties
+          ? 'S'
+          : game.extraTime
+            ? 'ÖT'
+            : null}
       </TableCell>
     </TableRow>
   )
