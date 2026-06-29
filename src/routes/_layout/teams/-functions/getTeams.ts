577039b91b4077397f89d24e3e8912343f7ebb07
DennisJosefsson <dennis.josefsson@gmail.com>
2026-06-29 12:32:26 +0200

    Implemented prepared queries

--- src/routes/_layout/teams/-functions/getTeams.ts
+++ src/routes/_layout/teams/-functions/getTeams.ts
@@ -1,9 +1,8 @@
-import { z } from 'zod'
-import { asc, sql } from 'drizzle-orm'
-import { zodValidator } from '@tanstack/zod-adapter'
-import { createServerFn } from '@tanstack/react-start'
 import { catchError } from '@/lib/middlewares/errors/catchError'
-import { db } from '@/db'
+import { createServerFn } from '@tanstack/react-start'
+import { zodValidator } from '@tanstack/zod-adapter'
+import { z } from 'zod'
+import { preparedTeamsList } from './preparedQueries/preparedTeamsList'
 
 const women = z.boolean()
 
@@ -11,14 +10,8 @@ export const getTeams = createServerFn({ method: 'GET' })
   .validator(zodValidator(women))
   .handler(async ({ data }) => {
     try {
-      const teamArray = await db.query.teams.findMany({
-        columns: {
-          teamId: true,
-          name: true,
-          casualName: true,
-        },
-        where: (teams, { eq, ne, and }) => and(eq(teams.women, data), ne(teams.teamId, 176)),
-        orderBy: [asc(sql`casual_name collate "se-SE-x-icu"`)],
+      const teamArray = await preparedTeamsList.execute({
+        women: data,
       })
 
       return teamArray
