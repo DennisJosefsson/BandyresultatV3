036a1b0031f1681db93511bbdff82d3bab04decd
DennisJosefsson <dennis.josefsson@gmail.com>
2026-07-02 13:51:23 +0200

    Temp fix clerk env variable

--- src/start.ts
+++ src/start.ts
@@ -8,9 +8,8 @@ import { dbErrorAdapter } from './lib/middlewares/errors/DbError'
 import { unauthorizedErrorAdapter } from './lib/middlewares/errors/UnauthorizedError'
 import { zodParsingErrorAdapter } from './lib/middlewares/errors/ZodParsingError'
 import { errorMiddleware } from './lib/middlewares/errors/errorMiddleware'
-import { serverEnv } from './lib/env/serverEnv'
 
-const SECRET_KEY = serverEnv.CLERK_SECRET_KEY
+const SECRET_KEY = process.env.CLERK_SECRET_KEY
 
 const csrfMiddleware = createCsrfMiddleware({
   filter: (ctx) => ctx.handlerType === 'serverFn',
