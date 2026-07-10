ba5f418b60cdf54d620687b549623231abde3398
DennisJosefsson <dennis.josefsson@gmail.com>
2026-07-10 14:28:18 +0200

    Minor design changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
+import {
+  useRouter,
+  useSearch,
+} from '@tanstack/react-router'
 import { SidebarIcon } from 'lucide-react'
-import { useRouter, useSearch } from '@tanstack/react-router'
-import ModeToggle from './ModeToggle'
-import { RouterBreadcrumb } from './Breadcrumb.tsx'
-import { Switch } from '../base/ui/switch'
-import { useSidebar } from '../base/ui/sidebar'
-import { Label } from '../base/ui/label'
 import { Button } from '../base/ui/button.tsx'
+import { Label } from '../base/ui/label'
+import { useSidebar } from '../base/ui/sidebar'
+import { Switch } from '../base/ui/switch'
+import { RouterBreadcrumb } from './Breadcrumb.tsx'
+import ModeToggle from './ModeToggle'
 
 const Header = () => {
   const { toggleSidebar } = useSidebar()
@@ -23,33 +26,41 @@ const Header = () => {
         women: !prev.women,
         teamId: undefined,
         opponentId: undefined,
+        teamArray: undefined,
       }),
     })
   }
 
   return (
     <header className="bg-primary-foreground sticky top-0 z-50 mb-2 flex w-full items-center border-b">
-      <div className="flex h-(--header-height) w-full items-center justify-between gap-2">
+      <div className="flex h-(--header-height) w-full items-center justify-between gap-2 ml-2">
         <div className="flex flex-row items-center gap-2">
-          <Button className="h-8 w-8" variant="ghost" size="icon" onClick={toggleSidebar}>
-            <SidebarIcon className="size-3 sm:size-4" />
+          <Button
+            variant="ghost"
+            size="icon-xs"
+            onClick={toggleSidebar}
+          >
+            <SidebarIcon />
           </Button>
           <RouterBreadcrumb />
         </div>
         <div className="mr-2 flex flex-row items-center gap-2 sm:gap-6">
-          <div className="flex w-full items-center space-x-1 sm:space-x-2">
-            <Switch
-              size="sm"
-              id="women"
-              defaultChecked={women}
-              checked={women}
-              onCheckedChange={updateWomen}
-            />
-            <Label className="text-[10px] md:text-xs lg:text-sm" htmlFor="women">
-              {women ? 'Herrar' : 'Damer'}
-            </Label>
-          </div>
-          <div className="mx-2">
+          <Switch
+            size="sm"
+            id="women"
+            defaultChecked={women}
+            checked={women}
+            onCheckedChange={updateWomen}
+          />
+
+          <Label
+            className="text-[7px] xs:text-[10px] md:text-xs lg:text-sm"
+            htmlFor="women"
+          >
+            {women ? 'Herrar' : 'Damer'}
+          </Label>
+
+          <div className="mr-2">
             <ModeToggle />
           </div>
         </div>
