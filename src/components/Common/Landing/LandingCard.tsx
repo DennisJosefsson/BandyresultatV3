46c0e2427c680f7f5f774ba81d463673e7257e17
DennisJosefsson <dennis.josefsson@gmail.com>
2026-07-20 16:40:11 +0200

    Minor changes, landing page

---
+++ src/components/Common/Landing/LandingCard.tsx
@@ -0,0 +1,34 @@
+import type { ReactNode } from 'react'
+
+const LandingCard = ({
+  children,
+}: {
+  children: ReactNode
+}) => {
+  return (
+    <div className="flex flex-col gap-2 border p-4 xl:gap-8 xl:p-10 shadow-lg">
+      {children}
+    </div>
+  )
+}
+
+function Gender({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-row justify-center">
+      <h4 className="text-xs xs:text-sm font-bold md:text-base xl:text-lg">
+        {children}
+      </h4>
+    </div>
+  )
+}
+
+function Content({ children }: { children: ReactNode }) {
+  return (
+    <div className="grid grid-cols-3 gap-8">{children}</div>
+  )
+}
+
+LandingCard.Gender = Gender
+LandingCard.Content = Content
+
+export default LandingCard
