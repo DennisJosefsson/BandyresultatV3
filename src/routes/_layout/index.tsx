6fd9427f7f2a4f6b0ea293f5540eb8672763f677
DennisJosefsson <dennis.josefsson@gmail.com>
2026-07-20 16:40:34 +0200

    Minor changes, landing page

--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -1,5 +1,9 @@
+import LandingCard from '@/components/Common/Landing/LandingCard'
 import type { LinkProps } from '@tanstack/react-router'
-import { Link, createFileRoute } from '@tanstack/react-router'
+import {
+  Link,
+  createFileRoute,
+} from '@tanstack/react-router'
 import { useGetFirstAndLastSeason } from './seasons/$year/-hooks/useGetFirstAndLastSeason'
 
 export const Route = createFileRoute('/_layout/')({
@@ -8,16 +12,17 @@ export const Route = createFileRoute('/_layout/')({
 
 function App() {
   return (
-    <div className="font-inter mx-auto mb-2 flex h-100 flex-col gap-2">
+    <div className="font-inter mx-2 sm:mx-8 mt-6 sm:mt-12 flex flex-col gap-2 max-h-80svh">
       <div className="my-10 flex flex-col xl:mx-10 2xl:mx-16">
         <div className="mb-3 md:mb-6">
-          <h1 className="text-primary pl-2 text-base font-bold sm:text-2xl xl:text-4xl">
+          <h1 className="text-primary pl-2 text-sm xs:text-base font-bold sm:text-2xl xl:text-4xl">
             Ett stycke bandyhistoria
           </h1>
         </div>
-        <div className="w-[280px] pl-2 md:w-[500px] xl:w-full">
-          <h2 className="text-primary mb-4 text-sm font-bold sm:text-base xl:text-2xl">
-            Samlade resultat från de högsta serierna - 1907 och framåt
+        <div className="w-70 pl-2 md:w-125 xl:w-full">
+          <h2 className="max-w-40 xs:max-w-fit text-primary mb-4 text-[10px] xs:text-sm font-bold sm:text-base xl:text-2xl">
+            Samlade resultat från de högsta serierna - 1907
+            och framåt
           </h2>
         </div>
       </div>
@@ -29,7 +34,9 @@ function App() {
 function IndexPageLinks() {
   const { lastSeason } = useGetFirstAndLastSeason()
 
-  const mensLinks: Array<LinkProps & { key: string; linkName: string }> = [
+  const mensLinks: Array<
+    LinkProps & { key: string; linkName: string }
+  > = [
     {
       to: '/seasons/$year/$group/games',
       params: { group: 'elitserien', year: lastSeason },
@@ -56,7 +63,9 @@ function IndexPageLinks() {
       linkName: 'Slutspel',
     },
   ]
-  const womensLinks: Array<LinkProps & { key: string; linkName: string }> = [
+  const womensLinks: Array<
+    LinkProps & { key: string; linkName: string }
+  > = [
     {
       to: '/seasons/$year/$group/games',
       params: { group: 'elitserien', year: lastSeason },
@@ -85,43 +94,57 @@ function IndexPageLinks() {
   ]
 
   return (
-    <div className="grid grid-cols-1 gap-2 xl:grid-cols-2">
-      <div className="border-muted flex flex-col gap-2 border-2 p-4 xl:gap-8 xl:p-10">
-        <div className="flex flex-row justify-center">
-          <h4 className="text-sm font-bold md:text-base xl:text-lg">Elitserien Herrar</h4>
-        </div>
-        <div className="grid grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 gap-4 sm:gap-12 xl:grid-cols-2">
+      <LandingCard>
+        <LandingCard.Gender>
+          Elitserien Herrar
+        </LandingCard.Gender>
+        <LandingCard.Content>
           {mensLinks.map((link) => {
             return (
-              <div key={link.key} className="flex flex-row justify-center">
-                <Link to={link.to} params={link.params} search={link.search}>
-                  <span className="text-xs font-semibold md:text-sm xl:text-base">
+              <div
+                key={link.key}
+                className="flex flex-row justify-center"
+              >
+                <Link
+                  to={link.to}
+                  params={link.params}
+                  search={link.search}
+                >
+                  <span className="text-[10px] xs:text-xs font-semibold md:text-sm xl:text-base">
                     {link.linkName}
                   </span>
                 </Link>
               </div>
             )
           })}
-        </div>
-      </div>
-      <div className="border-muted flex flex-col gap-2 border-2 p-4 xl:gap-8 xl:p-10">
-        <div className="flex flex-row justify-center">
-          <h4 className="text-sm font-bold md:text-base xl:text-lg">Elitserien Damer</h4>
-        </div>
-        <div className="grid grid-cols-3 gap-8">
+        </LandingCard.Content>
+      </LandingCard>
+      <LandingCard>
+        <LandingCard.Gender>
+          Elitserien Damer
+        </LandingCard.Gender>
+        <LandingCard.Content>
           {womensLinks.map((link) => {
             return (
-              <div key={link.key} className="flex flex-row justify-center">
-                <Link to={link.to} params={link.params} search={link.search}>
-                  <span className="text-xs font-semibold md:text-sm xl:text-base">
+              <div
+                key={link.key}
+                className="flex flex-row justify-center"
+              >
+                <Link
+                  to={link.to}
+                  params={link.params}
+                  search={link.search}
+                >
+                  <span className="text-[10px] xs:text-xs font-semibold md:text-sm xl:text-base">
                     {link.linkName}
                   </span>
                 </Link>
               </div>
             )
           })}
-        </div>
-      </div>
+        </LandingCard.Content>
+      </LandingCard>
     </div>
   )
 }
