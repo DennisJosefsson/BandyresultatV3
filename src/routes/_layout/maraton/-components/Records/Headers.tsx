5f0ca0503549e5978559918391d15de9bbcea960
DennisJosefsson <dennis.josefsson@gmail.com>
2026-08-07 12:47:32 +0200

    Fixed design bug

--- src/routes/_layout/maraton/-components/Records/Headers.tsx
+++ src/routes/_layout/maraton/-components/Records/Headers.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react'
 
 export function H1({ children }: { children: ReactNode }) {
   return (
-    <h1 className="max-w-50 text-xs font-semibold sm:text-sm lg:text-base">
+    <h1 className="text-xs font-semibold sm:text-sm lg:text-base">
       {children}
     </h1>
   )
@@ -10,7 +10,7 @@ export function H1({ children }: { children: ReactNode }) {
 
 export function H2({ children }: { children: ReactNode }) {
   return (
-    <h2 className="max-w-50 mb-1 sm:mb-2 text-[10px] xl:leading-4 font-semibold sm:text-xs lg:text-sm">
+    <h2 className="mb-1 sm:mb-2 text-[10px] xl:leading-4 font-semibold sm:text-xs lg:text-sm">
       {children}
     </h2>
   )
@@ -18,7 +18,7 @@ export function H2({ children }: { children: ReactNode }) {
 
 export function H3({ children }: { children: ReactNode }) {
   return (
-    <h3 className="max-w-50 mb-1 sm:mb-2 text-[10px] xl:leading-4 font-semibold sm:text-xs">
+    <h3 className="mb-1 sm:mb-2 text-[10px] xl:leading-4 font-semibold sm:text-xs">
       {children}
     </h3>
   )
