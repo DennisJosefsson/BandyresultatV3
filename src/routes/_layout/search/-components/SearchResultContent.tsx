9f5425d2fcb6b348d3edeb69dda3a09803b0c4dc
DennisJosefsson <dennis.josefsson@gmail.com>
2026-08-07 12:15:00 +0200

    Fixed search link

--- src/routes/_layout/search/-components/SearchResultContent.tsx
+++ src/routes/_layout/search/-components/SearchResultContent.tsx
@@ -1,25 +1,31 @@
-import { useCopyToClipboard } from 'usehooks-ts'
-import { useLocation } from '@tanstack/react-router'
-import type { SearchResult } from '@/lib/types/search'
-import { getBaseUrl } from '@/lib/utils/utils'
 import { Button } from '@/components/base/ui/button'
+import { clientEnv } from '@/lib/env/clientEnv'
+import type { SearchResult } from '@/lib/types/search'
+import { useLocation } from '@tanstack/react-router'
+import { useCopyToClipboard } from 'usehooks-ts'
 import ResultComponent from './ResultComponent'
 
 type SearchContentProps = { gameArray: Array<SearchResult> }
 
-const SearchContent = ({ gameArray }: SearchContentProps) => {
+const SearchContent = ({
+  gameArray,
+}: SearchContentProps) => {
   const [copiedText, copy] = useCopyToClipboard()
-  const baseUrl = getBaseUrl()
+
   const link = useLocation({
     select: (location) => location.href,
   })
 
-  const copyLink = `${baseUrl + link + '&submit=true'}`
+  const copyLink = `${clientEnv.VITE_SITE_PROD_URL + link + '&submit=true'}`
 
   return (
     <div className="mx-1 mt-4 xl:mx-0">
       <div className="flex flex-row-reverse gap-2">
-        <Button size="responsive" onClick={() => copy(copyLink)} className="w-30">
+        <Button
+          size="responsive"
+          onClick={() => copy(copyLink)}
+          className="w-30"
+        >
           {copiedText ? 'Kopierad!' : 'Länk till sökningen'}
         </Button>
       </div>
