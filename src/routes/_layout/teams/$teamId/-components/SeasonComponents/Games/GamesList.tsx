e09b4dda0ae407179f26cf0efc7baa6fcfa07f32
DennisJosefsson <dennis.josefsson@gmail.com>
2026-08-12 13:04:32 +0200

    Changed gamecard for team seasons game

--- src/routes/_layout/teams/$teamId/-components/SeasonComponents/Games/GamesList.tsx
+++ src/routes/_layout/teams/$teamId/-components/SeasonComponents/Games/GamesList.tsx
@@ -1,7 +1,5 @@
-import { Datum } from '@/components/Common/Date'
 import type { GroupGames } from '@/lib/types/game'
-import DataTable from './DataTable'
-import { columns } from './columns'
+import { GamesCard } from './GameCard'
 
 type GameListProps = {
   gamesArray: Array<GroupGames>
@@ -34,14 +32,6 @@ const GamesList = ({
               key={group.group}
               className="mb-6 w-full @container/teamseasongames"
             >
-              <div
-                id={group.group}
-                className="group mb-0.5 flex flex-row items-center gap-1 lg:mb-1 2xl:mb-2"
-              >
-                <h3 className="text-primary text-[10px] font-semibold tracking-wide md:text-xs xl:text-sm 2xl:text-base">
-                  {group.name}
-                </h3>
-              </div>
               <div className="ml-2 w-full table-fixed md:ml-4">
                 {group.comment && (
                   <span>{group.comment}</span>
@@ -49,19 +39,16 @@ const GamesList = ({
 
                 {group.dates.map((date) => {
                   return (
-                    <div
-                      key={date.date}
-                      className="mb-2 @lg:mb-4"
-                    >
-                      {date.date !== 'null' && (
-                        <span className="text-primary w-24 p-0 py-1 text-[10px] font-semibold tracking-wide md:text-xs xl:text-sm 2xl:text-base">
-                          <Datum>{date.date}</Datum>
-                        </span>
-                      )}
-                      <DataTable
-                        columns={columns}
-                        data={date.games}
-                      />
+                    <div key={date.date}>
+                      {date.games.map((game) => {
+                        return (
+                          <GamesCard
+                            key={game.gameId}
+                            game={game}
+                            serieName={group.name}
+                          />
+                        )
+                      })}
                     </div>
                   )
                 })}
