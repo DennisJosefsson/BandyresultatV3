6816f14a2ca8b9c173e16abf4cac225ee6bd21c5
DennisJosefsson <dennis.josefsson@gmail.com>
2026-08-17 12:44:17 +0200

    Button for removing teamseason on teamseason addition page, changed confirmation dialog text

--- src/routes/_layout/dashboard/-components/Season/TeamSeason.tsx
+++ src/routes/_layout/dashboard/-components/Season/TeamSeason.tsx
@@ -1,18 +1,30 @@
-import { useRef, useState } from 'react'
-import { getRouteApi } from '@tanstack/react-router'
 import ConfirmDialog from '@/components/Common/ConfirmDialog'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/base/ui/card'
 import { Button } from '@/components/base/ui/button'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/base/ui/card'
+import { getRouteApi } from '@tanstack/react-router'
+import { useRef, useState } from 'react'
 import { deleteTeamseasonMutation } from '../../-hooks/deleteTeamseasonMutation'
 
-const route = getRouteApi('/_layout/dashboard/season/$seasonId/')
+const route = getRouteApi(
+  '/_layout/dashboard/season/$seasonId/',
+)
 
 const TeamSeason = () => {
   const teams = route.useLoaderData({
     select: (s) => s.teams,
   })
   const dialogRef = useRef<HTMLDialogElement>(null)
-  const [teamseasonId, setTeamseasonId] = useState<number | null>(null)
+  const [teamseasonId, setTeamseasonId] = useState<
+    number | null
+  >(null)
+  const [teamName, setTeamName] = useState<string | null>(
+    null,
+  )
   const mutation = deleteTeamseasonMutation(dialogRef)
 
   const seasonId = route.useParams({
@@ -36,16 +48,21 @@ const TeamSeason = () => {
     <>
       <ConfirmDialog
         dialogRef={dialogRef}
-        onClose={() => setTeamseasonId(null)}
+        onClose={() => {
+          setTeamName(null)
+          setTeamseasonId(null)
+        }}
         confirmFunction={deleteTeamSeasonFunction}
         confirmButtonText="Ja, ta bort"
         closeButtonText="Nej,stäng"
-        confirmTitle="Är du säker på att du vill ta bort?"
+        confirmTitle={`Är du säker på att du vill ta bort teamSeason för ${teamName}?`}
       />
       <Card>
         <CardHeader>
           <div className="flex flex-row items-center justify-between">
-            <CardTitle className="xl:text-lg">Lag</CardTitle>
+            <CardTitle className="xl:text-lg">
+              Lag
+            </CardTitle>
             <div className="flex flex-row gap-2">
               <Button
                 render={
@@ -68,11 +85,19 @@ const TeamSeason = () => {
             <div>
               {teams.map((team) => {
                 return (
-                  <div key={team.teamId} className="mb-1 flex flex-row justify-between">
-                    <div className="xl:text-lg">{team.team.casualName}</div>
+                  <div
+                    key={team.teamId}
+                    className="mb-1 flex flex-row justify-between"
+                  >
+                    <div className="xl:text-lg">
+                      {team.team.casualName}
+                    </div>
                     <div className="flex flex-row gap-2">
                       <Button
-                        onClick={() => openDialog(team.teamseasonId)}
+                        onClick={() => {
+                          setTeamName(team.team.name)
+                          openDialog(team.teamseasonId)
+                        }}
                         size="sm"
                         variant="destructive"
                       >
--- src/routes/_layout/dashboard/-components/Season/TeamSeason/TeamSeasonAddition.tsx
+++ src/routes/_layout/dashboard/-components/Season/TeamSeason/TeamSeasonAddition.tsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import { getRouteApi } from '@tanstack/react-router'
-import { Input } from '@/components/base/ui/input'
 import { Button } from '@/components/base/ui/button'
+import { Input } from '@/components/base/ui/input'
+import ConfirmDialog from '@/components/Common/ConfirmDialog'
+import { getRouteApi } from '@tanstack/react-router'
+import { useRef, useState } from 'react'
 import { useAddTeamSeasonMutation } from '../../../-hooks/addTeamSeasonMutation'
+import { deleteTeamseasonMutation } from '../../../-hooks/deleteTeamseasonMutation'
 
-const route = getRouteApi('/_layout/dashboard/season/$seasonId/teamseason_/')
+const route = getRouteApi(
+  '/_layout/dashboard/season/$seasonId/teamseason_/',
+)
 
 const TeamSeasonAddition = () => {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+  const [teamseasonId, setTeamseasonId] = useState<
+    number | null
+  >(null)
+  const [teamName, setTeamName] = useState<string | null>(
+    null,
+  )
+  const mutation = deleteTeamseasonMutation(dialogRef)
   const { allTeams, teamSeasons } = route.useLoaderData()
   const women = route.useSearch({ select: (s) => s.women })
   const seasonId = route.useParams({
@@ -17,7 +29,12 @@ const TeamSeasonAddition = () => {
   const [teamFilter, setTeamFilter] = useState('')
   const teamSelection = allTeams
     .filter((team) => team.women === women)
-    .filter((team) => teamSeasons.findIndex((t) => t.teamId === team.teamId) === -1)
+    .filter(
+      (team) =>
+        teamSeasons.findIndex(
+          (t) => t.teamId === team.teamId,
+        ) === -1,
+    )
     .map((team) => {
       return {
         value: team.teamId,
@@ -26,7 +43,9 @@ const TeamSeasonAddition = () => {
     })
 
   const onClickTeamButton = (teamId: number) => {
-    const exist = teamSeasons.find((team) => team.teamId === teamId)
+    const exist = teamSeasons.find(
+      (team) => team.teamId === teamId,
+    )
     if (exist) return
 
     addTeamSeasonMutation.mutate({
@@ -34,11 +53,34 @@ const TeamSeasonAddition = () => {
     })
   }
 
+  const openDialog = (id: number) => {
+    setTeamseasonId(id)
+    dialogRef.current?.showModal()
+  }
+
+  const deleteTeamSeasonFunction = () => {
+    if (!teamseasonId) return
+    mutation.mutate({ data: { teamseasonId } })
+  }
+
   return (
     <div className="flex flex-col">
+      <ConfirmDialog
+        dialogRef={dialogRef}
+        onClose={() => {
+          setTeamName(null)
+          setTeamseasonId(null)
+        }}
+        confirmFunction={deleteTeamSeasonFunction}
+        confirmButtonText="Ja, ta bort"
+        closeButtonText="Nej,stäng"
+        confirmTitle={`Är du säker på att du vill ta bort teamSeason för ${teamName}?`}
+      />
       <div className="flex flex-row items-center justify-between">
         <div>
-          <h3 className="text-lg font-semibold">Lägg till lag</h3>
+          <h3 className="text-lg font-semibold">
+            Lägg till lag
+          </h3>
         </div>
         <div className="flex items-start justify-between p-5">
           <div className="flex items-center justify-end gap-2 p-6">
@@ -60,7 +102,9 @@ const TeamSeasonAddition = () => {
               placeholder="Filter"
               value={teamFilter}
               name="teamFilter"
-              onChange={(event) => setTeamFilter(event.target.value)}
+              onChange={(event) =>
+                setTeamFilter(event.target.value)
+              }
             />
           </div>
         </div>
@@ -68,10 +112,18 @@ const TeamSeasonAddition = () => {
       <div className="grid grid-cols-2">
         <div className="grid grid-cols-3 gap-8 place-self-start px-10">
           {teamSelection
-            .filter((team) => team.label.includes(teamFilter))
+            .filter((team) =>
+              team.label.includes(teamFilter),
+            )
             .map((team) => {
               return (
-                <Button key={team.value} size="sm" onClick={() => onClickTeamButton(team.value)}>
+                <Button
+                  key={team.value}
+                  size="sm"
+                  onClick={() =>
+                    onClickTeamButton(team.value)
+                  }
+                >
                   {team.label}
                 </Button>
               )
@@ -80,9 +132,21 @@ const TeamSeasonAddition = () => {
 
         <div className="flex flex-col gap-2 text-sm">
           <h6>Inlagda lag</h6>
-          <div className="grid grid-cols-2">
+          <div className="grid grid-cols-2 gap-x-4 gap-y-2">
             {teamSeasons.map((team) => {
-              return <div key={team.teamId}>{team.team.name}</div>
+              return (
+                <Button
+                  key={`${team.teamId.toString()}-added`}
+                  onClick={() => {
+                    setTeamName(team.team.name)
+                    openDialog(team.teamseasonId)
+                  }}
+                  size="sm"
+                  variant="destructive"
+                >
+                  {team.team.name}
+                </Button>
+              )
             })}
           </div>
         </div>
