8fa3454b287a93334b64eb5083d9c05fc7375f1a
DennisJosefsson <dennis.josefsson@gmail.com>
2026-09-03 14:29:28 +0200

    Refactored using intYear

--- src/routes/_layout/seasons/$year/-functions/getFirstAndLastSeasons.ts
+++ src/routes/_layout/seasons/$year/-functions/getFirstAndLastSeasons.ts
@@ -1,7 +1,9 @@
 import { db } from '@/db'
+import { seasons } from '@/db/schema'
 import { catchError } from '@/lib/middlewares/errors/catchError'
 import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
 import { createServerFn } from '@tanstack/react-start'
+import { max, min } from 'drizzle-orm'
 
 export const getFirstAndLastSeason = createServerFn({
   method: 'GET',
@@ -9,32 +11,26 @@ export const getFirstAndLastSeason = createServerFn({
   .middleware([errorMiddleware])
   .handler(async () => {
     try {
-      const firstSeason = await db.query.seasons
-        .findFirst({
-          columns: { year: true },
-          where: (seasons, { eq }) =>
-            eq(seasons.women, false),
-          orderBy: (seasons, { asc }) =>
-            asc(seasons.seasonId),
+      const seasonObject = await db
+        .select({
+          firstSeason: min(seasons.intYear),
+          lastSeason: max(seasons.intYear),
         })
-        .then((s) => {
-          if (!s) throw new Error('Ingen första-säsong!')
-          return Number(s.year)
+        .from(seasons)
+        .then((res) => {
+          const object = res[0]
+
+          if (!object.lastSeason || !object.firstSeason) {
+            throw new Error('Säsongsdata saknas')
+          }
+
+          return {
+            lastSeason: object.lastSeason,
+            firstSeason: object.firstSeason,
+          }
         })
 
-      const lastSeason = await db.query.seasons
-        .findFirst({
-          columns: { year: true },
-          where: (seasons, { eq }) =>
-            eq(seasons.women, false),
-          orderBy: (seasons, { desc }) =>
-            desc(seasons.seasonId),
-        })
-        .then((s) => {
-          if (!s) throw new Error('Ingen sista-säsong!')
-          return Number(s.year.split('/')[1])
-        })
-      return { firstSeason, lastSeason }
+      return { ...seasonObject }
     } catch (error) {
       catchError(error)
     }
