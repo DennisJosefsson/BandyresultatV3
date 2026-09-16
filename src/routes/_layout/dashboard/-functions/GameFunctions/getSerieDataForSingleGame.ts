09480b9e459a2071b3476908e2c29bdb8bf9a8bf
DennisJosefsson <dennis.josefsson@gmail.com>
2026-09-16 13:15:16 +0200

    Fixed query, join teamnames

--- src/routes/_layout/dashboard/-functions/GameFunctions/getSerieDataForSingleGame.ts
+++ src/routes/_layout/dashboard/-functions/GameFunctions/getSerieDataForSingleGame.ts
@@ -1,22 +1,25 @@
-import type { SQL } from 'drizzle-orm'
-import { eq } from 'drizzle-orm'
-import { createServerFn } from '@tanstack/react-start'
+import { db } from '@/db'
+import { teamnames, teams, teamseries } from '@/db/schema'
+import { catchError } from '@/lib/middlewares/errors/catchError'
+import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
 import type { TeamBase } from '@/lib/types/team'
 import { zd } from '@/lib/utils/zod'
-import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
-import { catchError } from '@/lib/middlewares/errors/catchError'
-import { teams, teamseries } from '@/db/schema'
-import { db } from '@/db'
+import { createServerFn } from '@tanstack/react-start'
+import type { SQL } from 'drizzle-orm'
+import { eq } from 'drizzle-orm'
 
 export const getSerieDataForSingleGame = createServerFn({
   method: 'GET',
 })
   .middleware([errorMiddleware])
-  .validator(zd.object({ serieId: zd.number().int().positive() }))
+  .validator(
+    zd.object({ serieId: zd.number().int().positive() }),
+  )
   .handler(async ({ data: { serieId } }) => {
     try {
       const serie = await db.query.series.findFirst({
-        where: (series, { eq: equal }) => equal(series.serieId, serieId),
+        where: (series, { eq: equal }) =>
+          equal(series.serieId, serieId),
         with: {
           season: {
             columns: { women: true },
@@ -30,13 +33,20 @@ export const getSerieDataForSingleGame = createServerFn({
         .select({
           team: {
             teamId: teams.teamId,
-            name: teams.name,
-            shortName: teams.shortName,
-            casualName: teams.casualName,
+            name: teamnames.name,
+            shortName: teamnames.shortName,
+            casualName: teamnames.casualName,
           } as unknown as SQL<TeamBase>,
         })
         .from(teamseries)
-        .leftJoin(teams, eq(teamseries.teamId, teams.teamId))
+        .leftJoin(
+          teams,
+          eq(teamseries.teamId, teams.teamId),
+        )
+        .leftJoin(
+          teamnames,
+          eq(teamnames.teamnameId, teams.teamnameId),
+        )
         .where(eq(teamseries.serieId, serieId))
         .then((res) => {
           const teamArray = res
