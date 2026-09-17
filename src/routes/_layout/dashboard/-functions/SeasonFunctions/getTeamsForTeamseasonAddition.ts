9097e57cef8f050816834c7a14d92164ba5e42e8
DennisJosefsson <dennis.josefsson@gmail.com>
2026-09-17 11:15:15 +0200

    Added joins on teamnames

--- src/routes/_layout/dashboard/-functions/SeasonFunctions/getTeamsForTeamseasonAddition.ts
+++ src/routes/_layout/dashboard/-functions/SeasonFunctions/getTeamsForTeamseasonAddition.ts
@@ -1,37 +1,52 @@
-import type { SQL } from 'drizzle-orm'
-import { asc, eq, getTableColumns, sql } from 'drizzle-orm'
-import { createServerFn } from '@tanstack/react-start'
+import { db } from '@/db'
+import { teamnames, teams, teamseasons } from '@/db/schema'
+import { catchError } from '@/lib/middlewares/errors/catchError'
+import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
 import type { TeamBase } from '@/lib/types/team'
 import { zd } from '@/lib/utils/zod'
-import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
-import { catchError } from '@/lib/middlewares/errors/catchError'
-import { teams, teamseasons } from '@/db/schema'
-import { db } from '@/db'
+import { createServerFn } from '@tanstack/react-start'
+import type { SQL } from 'drizzle-orm'
+import { asc, eq, getTableColumns, sql } from 'drizzle-orm'
 
-export const getTeamsForTeamseasonAddition = createServerFn({ method: 'GET' })
+export const getTeamsForTeamseasonAddition = createServerFn(
+  { method: 'GET' },
+)
   .middleware([errorMiddleware])
-  .validator(zd.object({ seasonId: zd.number().int().positive() }))
+  .validator(
+    zd.object({ seasonId: zd.number().int().positive() }),
+  )
   .handler(async ({ data: { seasonId } }) => {
     try {
       const allTeams = await db
         .select()
         .from(teams)
-        .orderBy(asc(sql`casual_name collate "se-SE-x-icu"`))
+        .orderBy(
+          asc(sql`casual_name collate "se-SE-x-icu"`),
+        )
 
       const teamSeasons = await db
         .select({
           ...getTableColumns(teamseasons),
           team: {
             teamId: teams.teamId,
-            name: teams.name,
-            shortName: teams.shortName,
-            casualName: teams.casualName,
+            name: teamnames.name,
+            shortName: teamnames.shortName,
+            casualName: teamnames.casualName,
           } as unknown as SQL<TeamBase>,
         })
         .from(teamseasons)
-        .leftJoin(teams, eq(teamseasons.teamId, teams.teamId))
+        .leftJoin(
+          teams,
+          eq(teamseasons.teamId, teams.teamId),
+        )
+        .leftJoin(
+          teamnames,
+          eq(teamnames.teamnameId, teams.teamnameId),
+        )
         .where(eq(teamseasons.seasonId, seasonId))
-        .orderBy(asc(sql`teams.casual_name collate "se-SE-x-icu"`))
+        .orderBy(
+          asc(sql`teams.casual_name collate "se-SE-x-icu"`),
+        )
 
       return { allTeams, teamSeasons }
     } catch (error) {
