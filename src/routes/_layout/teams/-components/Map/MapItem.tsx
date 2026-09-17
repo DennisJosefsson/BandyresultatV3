1f0abc079639eb1d8e31d661380b48174cc8839b
DennisJosefsson <dennis.josefsson@gmail.com>
2026-09-17 17:17:55 +0200

    Fixed type errors

--- src/routes/_layout/teams/-components/Map/MapItem.tsx
+++ src/routes/_layout/teams/-components/Map/MapItem.tsx
@@ -91,7 +91,7 @@ function MapItem({
           className="size-4 rounded-full border-2 border-orange-500 bg-orange-500 opacity-75 shadow-lg data-[state=checked]:animate-bounce data-[state=checked]:border-black data-[state=checked]:bg-black dark:data-[state=checked]:border-white dark:data-[state=checked]:bg-white"
         />
       </MarkerContent>
-      <MarkerTooltip>{team.name}</MarkerTooltip>
+      <MarkerTooltip>{team.teamname.name}</MarkerTooltip>
       <MarkerPopup
         className="data-[state=checked]:bg-primary/10 border w-40 xs:w-62 p-0"
         data-state={
@@ -102,17 +102,18 @@ function MapItem({
       >
         <div className="relative h-20 xs:h-32 overflow-hidden">
           <TeamLogo
-            teamId={team.teamId}
+            logoId={team.teamname.logo?.logoId}
+            hasDark={team.teamname.logo?.hasDark}
             size={256}
-            aria-label={team.name}
-            title={team.name}
+            aria-label={team.teamname.name}
+            title={team.teamname.name}
             className="object-cover -translate-y-5 xs:-translate-y-3.5"
           />
         </div>
         <div className="space-y-2 p-3">
           <div>
             <h3 className="text-foreground pb-0.5 text-[8px] xs:text-xs tracking-wide uppercase  font-semibold">
-              {team.name}
+              {team.teamname.name}
             </h3>
             <h4 className="text-[8px] xs:text-xs text-muted-foreground leading-tight font-medium">
               {`${team.city}, ${team.county.name}`}
