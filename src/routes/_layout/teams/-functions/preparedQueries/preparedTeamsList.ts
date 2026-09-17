56411095c86d0886ae78f5df96ed9039af2722ed
DennisJosefsson <dennis.josefsson@gmail.com>
2026-09-17 17:46:42 +0200

    Added joins on teamnames and teamlogos

--- src/routes/_layout/teams/-functions/preparedQueries/preparedTeamsList.ts
+++ src/routes/_layout/teams/-functions/preparedQueries/preparedTeamsList.ts
@@ -1,15 +1,14 @@
-import { asc, sql } from 'drizzle-orm'
 import { db } from '@/db'
+import { asc, sql } from 'drizzle-orm'
 
 export const preparedTeamsList = db.query.teams
   .findMany({
-    columns: {
-      teamId: true,
-      name: true,
-      casualName: true,
-    },
+    with: { teamname: { with: { logo: true } } },
     where: (teams, { eq, ne, and }) =>
-      and(eq(teams.women, sql.placeholder('women')), ne(teams.teamId, 176)),
+      and(
+        eq(teams.women, sql.placeholder('women')),
+        ne(teams.teamId, 176),
+      ),
     orderBy: [asc(sql`casual_name collate "se-SE-x-icu"`)],
   })
   .prepare('teamListQuery')
