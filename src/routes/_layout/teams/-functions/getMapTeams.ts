633a61ae286234274e869b9e01a42ab823e4bdaf
DennisJosefsson <dennis.josefsson@gmail.com>
2026-09-17 12:46:24 +0200

    Added joins on teamnames and teamlogos

--- src/routes/_layout/teams/-functions/getMapTeams.ts
+++ src/routes/_layout/teams/-functions/getMapTeams.ts
@@ -1,9 +1,9 @@
-import { z } from 'zod'
-import { asc, sql } from 'drizzle-orm'
-import { createServerFn } from '@tanstack/react-start'
-import type { MapTeam } from '@/lib/types/team'
-import { catchError } from '@/lib/middlewares/errors/catchError'
 import { db } from '@/db'
+import { catchError } from '@/lib/middlewares/errors/catchError'
+import type { MapTeam } from '@/lib/types/team'
+import { createServerFn } from '@tanstack/react-start'
+import { asc, sql } from 'drizzle-orm'
+import { z } from 'zod'
 
 const women = z.boolean()
 
@@ -16,12 +16,16 @@ export const getMapTeams = createServerFn({ method: 'GET' })
   .handler(async ({ data }) => {
     try {
       const mapTeams = await db.query.teams.findMany({
-        where: (teams, { eq, ne, and }) => and(eq(teams.women, data), ne(teams.teamId, 176)),
+        where: (teams, { eq, ne, and }) =>
+          and(eq(teams.women, data), ne(teams.teamId, 176)),
         with: {
           county: true,
           municipality: true,
+          teamname: { with: { logo: true } },
         },
-        orderBy: [asc(sql`casual_name collate "se-SE-x-icu"`)],
+        orderBy: [
+          asc(sql`casual_name collate "se-SE-x-icu"`),
+        ],
       })
 
       const sortedTeams = sortMapTeams(mapTeams)
