1fca0dbdf36465ff267ab3ebe7576bfffd7b6e29
DennisJosefsson <dennis.josefsson@gmail.com>
2026-09-18 10:29:39 +0200

    Changed add and edit functions to transactions

--- src/routes/_layout/dashboard/-functions/TeamFunctions/addTeam.ts
+++ src/routes/_layout/dashboard/-functions/TeamFunctions/addTeam.ts
@@ -1,21 +1,43 @@
-import { createServerFn } from '@tanstack/react-start'
-import { newTeam } from '@/lib/types/team'
-import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
-import { catchError } from '@/lib/middlewares/errors/catchError'
-import { authMiddleware } from '@/lib/middlewares/auth/authMiddleware'
-import { teams } from '@/db/schema'
 import { db } from '@/db'
+import { teamnames, teams } from '@/db/schema'
+import { authMiddleware } from '@/lib/middlewares/auth/authMiddleware'
+import { catchError } from '@/lib/middlewares/errors/catchError'
+import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
+import { newTeam } from '@/lib/types/team'
+import { createServerFn } from '@tanstack/react-start'
+import { eq } from 'drizzle-orm'
 
 export const addTeam = createServerFn({ method: 'POST' })
   .middleware([authMiddleware, errorMiddleware])
   .validator(newTeam)
   .handler(async ({ data }) => {
     try {
-      const returnTeam = await db.insert(teams).values(data).returning()
+      const teamnameId = await db.transaction(
+        async (tx) => {
+          const returnTeam = await tx
+            .insert(teams)
+            .values(data)
+            .returning()
+
+          return returnTeam[0].teamnameId
+        },
+      )
+      const teamName = await db
+        .select({ teamName: teamnames.name })
+        .from(teamnames)
+        .where(eq(teamnames.teamnameId, teamnameId))
+        .then((res) => {
+          if (res.length === 0) return undefined
+          return res[0].teamName
+        })
+
+      if (!teamName) {
+        throw new Error('Lagnamnet hittas inte.')
+      }
 
       return {
         status: 200,
-        message: `${returnTeam[0].name} lades till.`,
+        message: `Lag med namn ${teamName} lades till.`,
       }
     } catch (error) {
       catchError(error)
--- src/routes/_layout/dashboard/-functions/TeamFunctions/editTeam.ts
+++ src/routes/_layout/dashboard/-functions/TeamFunctions/editTeam.ts
@@ -1,26 +1,45 @@
-import { eq } from 'drizzle-orm'
-import { createServerFn } from '@tanstack/react-start'
-import { editTeamObject } from '@/lib/types/team'
-import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
-import { catchError } from '@/lib/middlewares/errors/catchError'
-import { authMiddleware } from '@/lib/middlewares/auth/authMiddleware'
-import { teams } from '@/db/schema'
 import { db } from '@/db'
+import { teamnames, teams } from '@/db/schema'
+import { authMiddleware } from '@/lib/middlewares/auth/authMiddleware'
+import { catchError } from '@/lib/middlewares/errors/catchError'
+import { errorMiddleware } from '@/lib/middlewares/errors/errorMiddleware'
+import { editTeamObject } from '@/lib/types/team'
+import { createServerFn } from '@tanstack/react-start'
+import { eq } from 'drizzle-orm'
 
 export const editTeam = createServerFn({ method: 'POST' })
   .middleware([authMiddleware, errorMiddleware])
   .validator(editTeamObject)
   .handler(async ({ data }) => {
     try {
-      const returnTeam = await db
-        .update(teams)
-        .set(data)
-        .where(eq(teams.teamId, data.teamId))
-        .returning()
+      const teamnameId = await db.transaction(
+        async (tx) => {
+          const returnTeam = await tx
+            .update(teams)
+            .set(data)
+            .where(eq(teams.teamId, data.teamId))
+            .returning()
+
+          return returnTeam[0].teamnameId
+        },
+      )
+
+      const teamName = await db
+        .select({ teamName: teamnames.name })
+        .from(teamnames)
+        .where(eq(teamnames.teamnameId, teamnameId))
+        .then((res) => {
+          if (res.length === 0) return undefined
+          return res[0].teamName
+        })
+
+      if (!teamName) {
+        throw new Error('Lagnamnet hittas inte.')
+      }
 
       return {
         status: 200,
-        message: `Information om ${returnTeam[0].name} ändrades.`,
+        message: `Information om ${teamName} ändrades.`,
       }
     } catch (error) {
       catchError(error)
