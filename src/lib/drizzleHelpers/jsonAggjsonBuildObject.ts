a7bc4ee297a2b11e2a8a26b05eebe76cc166a4b6
DennisJosefsson <dennis.josefsson@gmail.com>
2026-09-23 09:13:52 +0200

    Added helper to build json_agg and json_build_object

---
+++ src/lib/drizzleHelpers/jsonAggjsonBuildObject.ts
@@ -0,0 +1,96 @@
+import type { Column, SQL } from 'drizzle-orm'
+
+import { sql } from 'drizzle-orm'
+
+type Options = {
+  orderBy?: Array<SQL>
+  filter?: SQL
+}
+
+export function jsonAgg<T>(
+  expression: SQL,
+  options?: Options,
+) {
+  let orderBySql
+  if (
+    options &&
+    options.orderBy &&
+    options.orderBy.length > 0
+  ) {
+    orderBySql = sql` order by ${sql.join(options.orderBy, sql`, `)}`
+  }
+
+  let filterSql
+  if (options && options.filter) {
+    filterSql = sql` filter (where ${options.filter})`
+  }
+
+  return sql<T>`coalesce(json_agg(${expression}${orderBySql})${filterSql}, '[]'::json)`
+}
+
+/**
+ * @param shape Potential for SQL injections, so you shouldn't allow user-specified key names
+ */
+export function jsonBuildObject<
+  R,
+  T extends Record<string, Column | SQL> = Record<
+    string,
+    Column | SQL
+  >,
+>(shape: T) {
+  const chunks: Array<SQL> = []
+
+  Object.entries(shape).forEach(([key, value]) => {
+    if (chunks.length > 0) {
+      chunks.push(sql.raw(','))
+    }
+    chunks.push(sql.raw(`'${key}',`))
+    chunks.push(sql`${value}`)
+  })
+
+  return sql<R>`json_build_object(${sql.join(chunks)})`
+}
+
+export const jsonAggBuildObject = <
+  R,
+  T extends Record<string, Column | SQL> = Record<
+    string,
+    Column | SQL
+  >,
+>(
+  shape: T,
+  options?: Options,
+) => {
+  return jsonAgg<R>(jsonBuildObject(shape), options)
+}
+
+// const object = {
+//   teamId: teamgames.teamId,
+//   name: coalesce(teamseasonName.name, teamnames.name),
+//   shortName: coalesce(
+//     teamseasonName.shortName,
+//     teamnames.shortName,
+//   ),
+//   casualName: coalesce(
+//     teamseasonName.casualName,
+//     teamnames.casualName,
+//   ),
+//   logo: jsonBuildObject({
+//     logoId: coalesce(
+//       teamseasonLogo.logoId,
+//       teamlogos.logoId,
+//     ),
+//     hasDark: coalesce(
+//       teamseasonLogo.hasDark,
+//       teamlogos.hasDark,
+//     ),
+//   }),
+// }
+
+// const jsArrObj = jsonAggBuildObject(object, {
+//   orderBy: [
+//     desc(teamgames.teamId),
+//     asc(coalesce(teamseasonName.name, teamnames.name)),
+//   ],
+//   filter: isNotNull(teamgames.teamId),
+// })
